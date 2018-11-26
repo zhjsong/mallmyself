097625b641692de13618b4eba3d7801a36cd3752
zhjsong <492992198@qq.com>
2018-11-26 19:27:02 +0800

    '导入组件样式'

---
+++ src/components/login/login.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="login-wrap">
+      <el-form 
+      class="login-form"
+      label-position="top" 
+      label-width="80px" 
+      :model="formdata">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+      <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+      <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button type="success" class="login-btn">成功按钮</el-button>
+</el-form>
+    </div>
+</template>
+
+<script>
+export default {
+     data(){
+         return {
+             formdata:{
+                 username:'',
+                 password:''
+             }
+         }
+     }
+}
+</script>
+
+<style>
+</style>
